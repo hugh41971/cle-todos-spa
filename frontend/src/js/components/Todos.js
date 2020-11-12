d7964a3c02c64268d7d265b54637cea448326c10
Jen E <jenevans3ten@gmail.com>
2020-11-12 11:02:52 -0500

    11/12: Todos component with map, CORS issue, fetch from todos endpoint, fetch from public api

---
+++ frontend/src/js/components/Todos.js
@@ -0,0 +1,22 @@
+export default function Todos(todos){
+    return `
+        <h1>List of Todos</h1>
+        <ol>
+            ${todos.map(todo => {
+                return `
+                    <li>${todo.name} by ${todo.owner}</li>
+                `
+            }).join("")}
+        </ol>
+        <button class="todos__inspire">Inspire Me with Ron Swanson</button>
+    `
+
+    // return `
+    //     <h1>List of Todos</h1>
+    //     <ol>
+    //         <li>Buy groceries</li>
+    //         <li>Text Mom</li>
+    //         <li>Exercise</li>
+    //     </ol>
+    // `
+}
--- frontend/src/js/main.js
+++ frontend/src/js/main.js
@@ -1,12 +1,14 @@
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './components/Home';
+import Todos from './components/Todos';
 
 export default () => {
     // document.querySelector('.app').innerText = "Hello"
     header();
     footer();
     navHome();
+    navTodos();
 }
 
 const appDiv = document.querySelector('.app');
@@ -25,5 +27,34 @@ function navHome(){
     const homeButton = document.querySelector('.nav__home');
     homeButton.addEventListener('click', function(){
         appDiv.innerHTML = Home();
+    })
+}
+
+function navTodos(){
+    const todosButton = document.querySelector('.nav__todos');
+    todosButton.addEventListener('click', function(){
+        fetch("https://localhost:44393/api/todo")
+            .then(response => response.json())
+            .then(todos => {
+                appDiv.innerHTML = Todos(todos);
+                todosInspire();
+            })
+            .catch(err => console.log(err))
+    })
+}
+
+function todosInspire(){
+    const inspireButton = document.querySelector('.todos__inspire');
+    inspireButton.addEventListener('click', function(){
+        console.log("clicked the inspire button")
+        fetch("https://ron-swanson-quotes.herokuapp.com/v2/quotes")
+            .then(response => response.json())
+            .then(quote => {
+                inspireButton.remove();
+                const quoteElement = document.createElement('h3');
+                quoteElement.innerText = `${quote}`;
+                appDiv.appendChild(quoteElement);
+            })
+            .catch(err => console.log(err))
     })
 }
