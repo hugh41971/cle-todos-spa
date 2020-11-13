c4053d9c0a884da0af9222a53eda656db6aff512
Jen E <jenevans3ten@gmail.com>
2020-11-12 21:50:33 -0500

    added Owner model and relationships

---
+++ backend/todos/Repositories/OwnerRepository.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using todos.Models;
+
+namespace todos.Repositories
+{
+    public class OwnerRepository : Repository<Owner>, IRepository<Owner>
+    {
+        TodoContext db;
+
+        public OwnerRepository(TodoContext context) : base(context)
+        {
+            db = context;
+        }
+
+    }
+}
